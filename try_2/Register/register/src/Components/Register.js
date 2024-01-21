5b6d96d912327f3e16689e55794fea399ee8f2f9
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-21 18:09:39 +0530

    Now, h-100 means this component gives full height of its parent component.... & vh-100 means component gives full height of window of screen... & 'row' means select one row for this component and 'col' means row divide into same columns parts...

--- try_2/Register/register/src/Components/Register.js
+++ try_2/Register/register/src/Components/Register.js
@@ -4,7 +4,18 @@ function Register(){
     return(
         <section className="vh-100 bg-light">
             <div className="h-100">
-                <h1>Hello</h1>
+                <div className="row h-100">
+                    <div className="col bg-primary h-100">
+                        <h1>Hello...</h1>
+                        <h1>Hello...</h1>
+                    </div>
+                    <div className="col">
+                        <h1>Hi...</h1>
+                    </div>
+                    <div className="col">
+                        <h1>Bonjure...</h1>
+                    </div>
+                </div>
             </div>
         </section>
     )
