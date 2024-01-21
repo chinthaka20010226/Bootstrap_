52f017915463501458bac5a3d02c00853d6832b2
chinthaka20010226 <prabathchinthaka361@gmail.com>
2024-01-21 18:55:49 +0530

    Now, a component define d-flex, it means its child component arrange in a row(default), then define justify-content-center, its means child components center on Main axis... & define align-items-center, its means child components center on another axis, which is perpendicular to Main axis...

--- try_2/Register/register/src/Components/Register.js
+++ try_2/Register/register/src/Components/Register.js
@@ -5,15 +5,18 @@ function Register(){
         <section className="vh-100 bg-light">
             <div className="h-100">
                 <div className="row h-100">
-                    <div className="col bg-primary h-100">
-                        <h1>Hello...</h1>
-                        <h1>Hello...</h1>
-                    </div>
                     <div className="col">
-                        <h1>Hi...</h1>
-                    </div>
-                    <div className="col">
-                        <h1>Bonjure...</h1>
+                        <div className="card card-registration h-100">
+                            <div className="row h-100">
+                                <div className="col-md-6 bg-info d-flex flex-column justify-content-center align-items-center">
+                                    <h1>Welcome Expensy</h1>
+                                    <p>Effortlessly manage your finances with our intuitive expense tracker. Stay in control, track spending,
+                                        and achive financial goals seamlessly. Welcome to a smart way to handle your expenses.
+                                    </p>
+                                </div>
+                                <div className="col-md-6"></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
